2d6e5faad63d791f524ba6b6f16d87a2ceac3d35
sasi080900 <42990827+sasi080900@users.noreply.github.com>
2018-09-19 10:36:07 +0530

    Add files via upload

---
+++ snake&lader game.py
@@ -0,0 +1,14 @@
+#lets start a game 
+
+import random
+
+#lets p is the position of the player 
+p=0
+
+
+#loops 
+while True 
+     r= print(random.randint(1,6))
+     p+=r
+     if (p==8):
+     return p=37   
