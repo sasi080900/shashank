f78f7ecc508c00ee1e0d8d98b2d2472c6c116b5e
sasi080900 <42990827+sasi080900@users.noreply.github.com>
2018-10-01 14:14:56 +0530

    Update snakelader.py

--- snakelader.py
+++ snakelader.py
@@ -48,3 +48,80 @@ while(count<=100):
 		if(count+r>100):
 		   count=count-r 
 		   print("you can't go forward throw the dice again////")   
+			OUTPUT
+	press r to roll the dice and q to quit the game r
+u got  3
+new position is  3
+press r to roll the dice and q to quit the game r
+u got  3
+new position is  6
+press r to roll the dice and q to quit the game r
+u got  1
+new position is  7
+press r to roll the dice and q to quit the game r
+u got  3
+new position is  10
+press r to roll the dice and q to quit the game r
+u got  6
+new position is  16
+press r to roll the dice and q to quit the game r
+u got  6
+new position is  22
+press r to roll the dice and q to quit the game r
+u got  1
+new position is  23
+press r to roll the dice and q to quit the game 
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  28
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  33
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  38
+press r to roll the dice and q to quit the game r
+u got  4
+new position is  42
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  47
+press r to roll the dice and q to quit the game r
+u got  1
+new position is  48
+press r to roll the dice and q to quit the game r
+u got  4
+new position is  52
+you git a ladder u r current position is 81
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  86
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  91
+press r to roll the dice and q to quit the game r
+u got  5
+new position is  96
+you can't go forward throw the dice again////
+press r to roll the dice and q to quit the game r
+u got  6
+new position is  97
+you can't go forward throw the dice again////
+press r to roll the dice and q to quit the game r
+u got  3
+new position is  94
+press r to roll the dice and q to quit the game r
+u got  3
+new position is  97
+press r to roll the dice and q to quit the game r
+u got  4
+new position is  101
+you can't go forward throw the dice again////
+press r to roll the dice and q to quit the game r
+u got  1
+new position is  98
+press r to roll the dice and q to quit the game r
+u got  2
+new position is  100
+you won the game 
+		
